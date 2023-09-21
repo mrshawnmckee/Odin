3c59c1184950e0543448144d11a890f55117a9f9
Shawn McKee <shawnroosemckee@gmail.com>
2023-09-20 22:39:25 -0400

    worked on initial calc setup at JS/calc

---
+++ JSPrac/Calculator/script.js
@@ -0,0 +1,16 @@
+const doMath = function(num1, num2, operator){
+    let total;
+    if(operator === "+") {
+        total = num1 + num2
+    }
+    if(operator === "-") {
+        total = num1 - num2
+    }
+    if(operator === "*") {
+        total = num1 * num2
+    }
+    if(operator === "/") {
+        total = num1 / num2
+    }
+
+}
